b7ccbe4dcb89ac4a96e195e0fe8a08ec4ec1d1c6
Aaron Ellington <aaron@ellington.io>
2022-10-20 09:58:09 -0400

    wip

--- internal/forge/assert.go
+++ internal/forge/assert.go
@@ -1,14 +1,26 @@
 package forge
 
 import (
+	"encoding/json"
 	"fmt"
 	"reflect"
 )
 
+// AssertFailure is foobar
+type AssertFailure struct {
+	Expected interface{}
+	Actual   interface{}
+}
+
 // Assert is foobar
 func Assert(expected interface{}, actual interface{}) error {
 	if !reflect.DeepEqual(expected, actual) {
-		return fmt.Errorf("not equal... Got: %+v, Expected: %+v", actual, expected)
+		actualBytes, _ := json.Marshal(AssertFailure{
+			Expected: expected,
+			Actual:   actual,
+		})
+
+		return fmt.Errorf("not equal... %s", string(actualBytes))
 	}
 
 	return nil
