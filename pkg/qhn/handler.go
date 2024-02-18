6850636191957b8c8702d4e97fab66a0975b0acd
Aaron Ellington <aaron@ellington.io>
2024-02-17 21:28:37 -0500

    wip

--- pkg/qhn/handler.go
+++ pkg/qhn/handler.go
@@ -2,17 +2,24 @@ package qhn
 
 import (
 	"net/http"
+	"time"
 
+	"github.com/aaronellington/quiet-hacker-news/pkg/hackernews"
 	"github.com/kyberbits/forge/forge"
 )
 
-type APIResponse struct {
-	Message string `json:"message"`
+type TemplatePayload struct {
+	Items  []hackernews.Item
+	Uptime string
 }
 
 func (app *App) handlerRoot() http.Handler {
+	now := time.Now()
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		handlerContext := forge.NewHandlerContext(w, r)
-		handlerContext.ExecuteTemplate(app.resources.Index, app.hackernewsItems)
+		handlerContext.ExecuteTemplate(app.resources.Index, TemplatePayload{
+			Items:  app.hackernewsItems,
+			Uptime: time.Since(now).Round(time.Second).String(),
+		})
 	})
 }
